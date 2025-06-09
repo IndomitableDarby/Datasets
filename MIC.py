86161ff89682ea87c5e6b8d595213b28a9d3cdf2
IndomitableDarby <91589264+IndomitableDarby@users.noreply.github.com>
2025-06-09 16:39:22 +0530

    Update MIC.py

--- MIC.py
+++ MIC.py
@@ -168,7 +168,7 @@ def process_dataset(name, df, label_col):
         print(f"{int(frac*100):<10}{n_samples:<10}{res['runtime_sec']:<15}{res['accuracy_percent']:<17}{res['avg_convergence_percent']}")
 
 # === Load Boston Housing Dataset ===
-df_boston = pd.read_csv("/content/sample_data/california_housing_test.csv")
+df_boston = pd.read_csv("/content/sample_data/BostonHousing.csv")
 process_dataset("Boston Housing", df_boston, label_col="medv")
 
 # === Load Parkinson’s Dataset ===
